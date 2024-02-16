ef0f1f1b8ccb443d1b94c9df06cfbe636e107df2
Arka90 <skarka90@gmail.com>
2024-02-16 18:46:47 +0530

    done with news api

--- controllers/getTopNewsController.js
+++ controllers/getTopNewsController.js
@@ -1,9 +1,13 @@
+const axios = require("axios");
 const catchAsync = require("../utils/catchAsync");
 
 exports.getTopNews = catchAsync(async (req, res) => {
-  const news = fetch(
-    `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${process.env.NEWS_API_KEY}`
-  );
+  const options = {
+    method: "GET",
+    url: `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${process.env.NEWS_API_KEY}`,
+  };
 
-  return res.status(200).json({ news });
+  const news = await axios.request(options);
+
+  return res.status(200).json({ data: news.data.articles });
 });
